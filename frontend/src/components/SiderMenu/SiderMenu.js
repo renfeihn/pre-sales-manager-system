da18be18be2aa069747faf06a1c9f804bad7d848
renfei <renfei_hn@163.com>
2018-05-04 18:37:39 +0800

    修改系统title

--- frontend/src/components/SiderMenu/SiderMenu.js
+++ frontend/src/components/SiderMenu/SiderMenu.js
@@ -215,7 +215,7 @@ export default class SiderMenu extends PureComponent {
         <div className={styles.logo} key="logo">
           <Link to="/">
             <img src={logo} alt="logo" />
-            <h1>Ant Design Pro</h1>
+            <h1>产品售前管理系统</h1>
           </Link>
         </div>
         <Menu
